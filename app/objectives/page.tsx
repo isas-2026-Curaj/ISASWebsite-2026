f84ac197316281a61db3beda6a537d6b8bcf6350
isas-2026-Curaj <isas@curaj.ac.in>
2025-10-30 19:49:41 +0530

    Update page.tsx

--- app/objectives/page.tsx
+++ app/objectives/page.tsx
@@ -4,13 +4,13 @@ export default function ObjectivesPage() {
   return (
     <main>
       <Section title="Motivation and Objectives">
-         <p className="text-xs text-muted-foreground tracking-wider">
+         <p className="text-muted-foreground tracking-wider">
               Agriculture is at the core of India’s socio-economic structure. However, farmers often face challenges due to climate variability, market volatility and limited access to scientific knowledge. 
            Agricultural statistics and data science can bridge these gaps by enabling evidence-based decisions, strengthening extension services and guiding innovations at the grassroots level. 
            Agricultural statistics play a critical role in evidence-based policymaking, resource allocation and technology adoption. With growing emphasis on data-driven solutions, this Conference will foster dialogue on statistical methodologies, big data applications, climate-smart agriculture and the integration of digital technologies in farming systems. 
            The event aligns with India’s vision of promoting sustainable agriculture, rural development and food security. 
          </p>
-        <p className="text-xs text-muted-foreground tracking-wider">
+        <p className="text-muted-foreground tracking-wider">
 The inclusion of farmers, students, and KVK representatives ensures that the knowledge generated during the Conference translates into real-world benefits by empowering the farming community, building capacity in young researchers and enhancing the effectiveness of KVK-led extension services.
             </p>
         <ul className="grid md:grid-cols-1 gap-3 list-disc pl-5">
