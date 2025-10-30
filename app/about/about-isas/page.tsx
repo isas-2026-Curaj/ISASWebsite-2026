539e6aee20230795a03bf61690c74d473b74f1ff
isas-2026-Curaj <isas@curaj.ac.in>
2025-10-30 21:24:22 +0530

    Create page.tsx

---
+++ app/about/about-isas/page.tsx
@@ -0,0 +1,16 @@
+export default function Page() {
+  return (
+    <main className="container mx-auto max-w-5xl px-4 py-10">
+      <h1 className="text-3xl font-bold">Privacy Policy</h1>
+      <p className="mt-4 text-muted-foreground">
+        India has a proud legacy of statisticians who have made significant contributions to national and international welfare, establishing a strong foundation for Agricultural Statistics. To recognize these contributions, the Indian Society of Agricultural Statistics (ISAS) was founded on January 3, 1947, with Dr. Rajendra Prasad as its Founder President, who continued to guide the Society for 16 years even after becoming the President of Republic of India.
+      </p>
+      <p className="mt-4 text-muted-foreground">
+       The Society’s primary objective has been to disseminate research in Agricultural Statistics and informatics to address challenges in agriculture. It publishes the “Journal of the Indian Society of Agricultural Statistics” (ISSN 0019-6363), a unique platform for applied statistical research aimed at enhancing agricultural research and development across India.
+       </p>
+      <p className="mt-4 text-muted-foreground">
+       Continuing its mission, ISAS, in collaboration with the Department of Statistics, Central University of Rajasthan, Ajmer, is organizing the Annual Conference 2026 on Data Driven Decisions for Connecting Science with Society towards Viksit Bharat @ 2047, providing a forum for statisticians and researchers to share insights and innovations.
+</p> 
+    </main>
+  )
+}
